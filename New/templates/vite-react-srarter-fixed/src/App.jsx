c0b05ceb4951a62a4f5963f035dc7349e2cfbd66
Siegerus <ivanoffsiegerus@gmail.com>
2025-09-05 09:49:54 +0300

    1

---
+++ New/templates/vite-react-srarter-fixed/src/App.jsx
@@ -0,0 +1,54 @@
+import './App.scss';
+
+function App() {
+	return (
+		<div className="app">
+
+		</div>
+	);
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* let App = () => {
+  let [buttonClass, buttonSetClass] = React.useState('app__button');
+  function onButtonClick() {
+	buttonSetClass('app__button app__button_green');
+  }
+  return (
+	<div className="app">
+	  <button className={buttonClass} onClick={onButtonClick}>
+		<span>Click!</span>
+	  </button>
+	</div>
+  );
+};
+
+export default App; */
