91923f9f9ff99843d8888cdc057a48e82de1f9c8
Siegerus <ivanoffsiegerus@gmail.com>
2025-09-30 07:27:12 +0300

    first libRedux commit

--- New/ReactJs/vite-react-BooksLib-Redux/src/App.jsx
+++ New/ReactJs/vite-react-BooksLib-Redux/src/App.jsx
@@ -4,7 +4,9 @@ import './App.scss';
 function App() {
 	return (
 		<div className="app">
-
+			<header className="app-header">
+				<h1>Book Library App</h1>
+			</header>
 		</div>
 	);
 }
--- New/ReactJs/vite-react-BooksLib-Redux/src/context/UserContext.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-let UserContext = createContext({ // В скобках можно указать значение по умолчанию. Будет указываться, если нет "UserContext.Provider"
-    UserName: '',
-    ChangeUserName: () => {},   // Указывается как правило хорошего тона, что бы, если гдето нет "Provider", небыло ошибки "not a function"
-}); 
-
-export default UserContext; 
