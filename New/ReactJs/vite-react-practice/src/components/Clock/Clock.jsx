174bba0e2b78ceb8c299b558da89dbfea7185367
Siegerus <ivanoffsiegerus@gmail.com>
2025-10-01 20:51:55 +0300

    1

--- New/ReactJs/vite-react-practice/src/components/Clock/Clock.jsx
+++ New/ReactJs/vite-react-practice/src/components/Clock/Clock.jsx
@@ -1,21 +1,18 @@
 import { useState } from 'react';
 import ClockBox from './ClockBox';
 import ClockAction from './ClockAction';
+import { useEffect } from 'react';
 
 function Clock() {
-	let [values, setValues] = useState({
-		hours: '00',
-		minutes: '00',
-		seconds: '00',
-		isTick: true
-	});
+	console.log('render');
+
 	let date = new Date();
 	let interval;
 
 	let getTime = () => {
-		let hours = date.getHours();
-		let minutes = date.getMinutes();
-		let seconds = date.getSeconds();
+		let hours = date.getHours().toString().padStart(2, '0');
+		let minutes = date.getMinutes().toString().padStart(2, '0');
+		let seconds = date.getSeconds().toString().padStart(2, '0');
 
 		let timeObject = {
 			hours: hours,
@@ -24,6 +21,13 @@ function Clock() {
 		};
 		return timeObject;
 	};
+
+	let [values, setValues] = useState({
+		hours: getTime().hours,
+		minutes: getTime().minutes,
+		seconds: getTime().seconds,
+		isTick: true
+	});
 
 	let setTimes = ({ hours, minutes, seconds }, isTick) => {
 		clearInterval(interval);
@@ -34,12 +38,12 @@ function Clock() {
 			isTick
 		});
 	};
-
+	
 	if (values.isTick) {
 		interval = setInterval(() => {
 			setTimes(getTime(), true);
 		}, 1000);
-	}
+	} else clearInterval(interval);
 
 	let clearInt = () => {
 		if (values.isTick) {
