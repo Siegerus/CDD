a7e5d32a0b1a67cc3305d708febbd53696ccff9e
Siegerus <ivanoffsiegerus@gmail.com>
2025-10-03 19:38:49 +0300

    1

--- New/ReactJs/vite-react-BooksLib-Redux/src/redux/slices/filterSlice.js
+++ New/ReactJs/vite-react-BooksLib-Redux/src/redux/slices/filterSlice.js
@@ -3,14 +3,16 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
     title: ''
 }
+
 const filterSlice = createSlice({
     name: 'filter',
     initialState,
-    reduсers: {
-        setTitleFilter: (state, action) => {           // редьюсер
-            return {...state, title: action.payload}
+    reducers: {
+        setTitleFilter: (state, action) => {        // редьюсер
+            return {...state, title: action.payload }
         }
     }
 })
-console.log(filterSlice)
-export default filterSlice.reducer;      // св-во слайса, в котором находится редьюсер
+
+console.log(filterSlice.actions);
+export default filterSlice.reducer;  // св-во слайса, в котором находится редьюсер
